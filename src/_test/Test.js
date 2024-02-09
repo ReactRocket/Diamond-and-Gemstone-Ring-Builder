57989a37e8c97fa4f980ae2be3e4b350afc43a65
AYUSH VARMA <153015142+ReactRocket@users.noreply.github.com>
2024-02-09 17:41:33 +0530

    Breadcrumb Dynamically changed

--- src/_test/Test.js
+++ src/_test/Test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Slider from "../components/Slider";
+import DoubleRangeSlider from "../components/Slider";
 
 const Test = () => {
   const handleRangeChange = (newRange) => {
@@ -9,16 +9,7 @@ const Test = () => {
   return (
     <div>
       <h1>Test Mode</h1>
-      <div className="container mx-auto py-8">
-        <h1>Price Range Slider</h1>
-        <Slider
-          minPrice={100} // Minimum allowed price
-          maxPrice={1000} // Maximum allowed price
-          initialMin={200} // Initial minimum value
-          initialMax={800} // Initial maximum value
-          onChange={handleRangeChange}
-        />
-      </div>
+      <DoubleRangeSlider />
     </div>
   );
 };
--- src/components/AllFilters.js
+++ src/components/AllFilters.js
@@ -1,11 +1,35 @@
 import React from "react";
 import Breadcrumb from "./Breadcrumb";
 import AdvancedFilter from "./AdvancedFilter";
+import img1 from "../assets/images/breadcrum_icons/diamond-ring.png";
+import img2 from "../assets/images/breadcrum_icons/diamond.png";
+import img3 from "../assets/images/breadcrum_icons/engagement-ring.png";
+
+const data = [
+  {
+    number: 1,
+    text1: "Choose a",
+    text2: "Diamond",
+    img: img2,
+  },
+  {
+    number: 2,
+    text1: "Choose a",
+    text2: "Setting",
+    img: img3,
+  },
+  {
+    number: 3,
+    text1: "Review",
+    text2: "Complete",
+    img: img1,
+  },
+];
 
 const AllFilters = () => {
   return (
     <div className="px-10 pb-10">
-      <Breadcrumb />
+      <Breadcrumb data={data} />
       <div className=" flex justify-center items-center py-7 w-full">
         <button className="w-1/4 bg-[#6d3bae] border-[#6d3bae] border-2 text-white p-2">
           MINED
