ac4812f1bb140f5c83c2de147057f38adf1ecdc1
AYUSH VARMA <153015142+ReactRocket@users.noreply.github.com>
2024-02-14 19:15:47 +0530

    Product Filter Added & Dual Slider Created

--- src/components/AllFilters.js
+++ src/components/AllFilters.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Breadcrumb from "./Breadcrumb";
 import AdvancedFilter from "./AdvancedFilter";
 import img1 from "../assets/images/breadcrum_icons/diamond-ring.png";
 import img2 from "../assets/images/breadcrum_icons/diamond.png";
 import img3 from "../assets/images/breadcrum_icons/engagement-ring.png";
+import ProductFilter from "./ProductFilter";
 
 const data = [
   {
@@ -26,9 +27,11 @@ const data = [
   },
 ];
 const AllFilters = () => {
+  const [advanceFilterToggle, setAdvanceFilterToggle] = useState(false);
+
   return (
     <div className="px-10 pb-10">
-      <Breadcrumb data={data}  />
+      <Breadcrumb data={data} />
       <div className=" flex justify-center items-center py-7 w-full">
         <button className="w-1/4 bg-[#6d3bae] border-[#6d3bae] border-2 text-white p-2">
           MINED
@@ -37,12 +40,36 @@ const AllFilters = () => {
           LAB CREATED
         </button>
       </div>
-      <AdvancedFilter />
-      <div className=" flex justify-center items-center py-7 w-full">
-        <button className="w-1/5 bg-[#6d3bae] text-white p-2">
+      <AdvancedFilter advanceFilterToggle={advanceFilterToggle} />
+      <div className=" flex justify-between items-center py-7 w-full">
+        <span>&nbsp;</span>
+        <button
+          onClick={() => setAdvanceFilterToggle(!advanceFilterToggle)}
+          className="w-1/5 bg-[#6d3bae] text-white p-2"
+        >
           ADVANCED FILTERS
         </button>
+        <span className="flex justify-center items-center gap-2 text-gray-400 cursor-pointer">
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-arrow-repeat"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9" />
+              <path
+                fill-rule="evenodd"
+                d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"
+              />
+            </svg>
+          </span>
+          <span> Reset All</span>{" "}
+        </span>
       </div>
+      <ProductFilter />
     </div>
   );
 };
