b5cbca011405fe4a6eb271434b8abc6ea83054e5
AYUSH VARMA <153015142+ReactRocket@users.noreply.github.com>
2024-02-26 23:14:59 +0530

    Firebase Hosted And 404 page updated (Redirecting in 10 seconds)

--- src/pages/error/Error.js
+++ src/pages/error/Error.js
@@ -1,15 +1,30 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const Error = () => {
+  const [Timer, setTimer] = useState(10);
   const location = useLocation();
+  const navigate = useNavigate();
 
-  const path = location.pathname ; 
+  setTimeout(() => {
+    setTimer(Timer > 0 && Timer - 1);
+  }, 1000);
+
+  useEffect(() => {
+    if (Timer === 0) {
+      navigate(-1);
+    }
+  }, [Timer]);
+
+  const path = location.pathname;
   return (
     <>
-     <h1 className="text-[#7646D7] text-[52px] font-sans p-10">
-      {`"${path.slice(1,)}" Not Found`}
-    </h1>
+      <h1 className="text-[#7646D7] text-[52px] font-sans p-10">
+        {`"${path.slice(1)}" Page Not Found`}
+      </h1>
+      <h2 className="text-[#7646D7] text-lg font-sans p-10">
+        Redirecting in <b> ({Timer}) </b> seconds to the previous page...
+      </h2>
     </>
   );
 };
