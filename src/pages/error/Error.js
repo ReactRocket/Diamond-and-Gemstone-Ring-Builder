3dc22d1152c9a4294606d55c8e202e372fb81fa1
AYUSH VARMA <153015142+ReactRocket@users.noreply.github.com>
2024-02-27 15:35:35 +0530

    Error page updated(Redirecting time changed from 10 sec to 5 sec.

--- src/pages/error/Error.js
+++ src/pages/error/Error.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 export const Error = () => {
-  const [Timer, setTimer] = useState(10);
+  const [Timer, setTimer] = useState(5);
   const location = useLocation();
   const navigate = useNavigate();
 
