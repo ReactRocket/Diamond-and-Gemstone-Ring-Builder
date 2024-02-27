7f6bed075fed04f5d9854a51b2d1b6ad0a79052d
AYUSH VARMA <153015142+ReactRocket@users.noreply.github.com>
2024-02-27 17:49:45 +0530

    Testing Feature Updated( When Testing mode is off then loader disabled {localStorage.setItem("testing",true)}

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,6 +1,6 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 
-const Loader = ({isLoading}) => {
+const Loader = ({ isLoading }) => {
   useEffect(() => {
     document.querySelector("body").style.overflow = "hidden";
 
@@ -16,8 +16,8 @@ const Loader = ({isLoading}) => {
   }, [isLoading]);
 
   return (
-    <div className="z-50 h-full w-screen  bg-opacity-35 fixed top-0 left-0  flex justify-center items-center">
-      <span className=" w-[100px] aspect-square border-transparent  rounded-full border-[6px] animate-spin border-t-[#7646D7]"></span>
+    <div className="transition-all duration-1000 z-50 h-full w-screen  bg-opacity-35 fixed top-0 left-0  flex justify-center items-center">
+      <span className=" w-[100px] aspect-square delay-1000 border-transparent  rounded-full border-[6px] animate-spin border-t-[#7646D7]"></span>
     </div>
   );
 };
