e950998eb16d51b917517cda5a3f4a4adfa091f0
AYUSH VARMA <153015142+ReactRocket@users.noreply.github.com>
2024-03-01 18:58:01 +0530

    Refactor DiamondsChild component. Remove unnecessary components and replace ProductChild with ZoomView.

--- src/_test/Test.js
+++ src/_test/Test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ProductChild from "../components/ProductChild";
+import ZoomView from "../components/ZoomView";
 
 
 const Test = () => {
@@ -7,7 +8,8 @@ const Test = () => {
     // <div className="h-screen w-screen flex justify-center items-center">
       <div>
         {/* <h1>Test Mode</h1> */}
-        <ProductChild/>
+        {/* <ProductChild/> */}
+        <ZoomView/>
       </div>
     // </div>
   );
---
+++ src/components/RecentlyViewed.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RecentlyViewed = () => {
+  return (
+    <div>RecentlyViewed</div>
+  )
+}
+
+export default RecentlyViewed
