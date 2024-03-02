c9ae7ffd5bbbc8a4f7f492055cff408c7af7dc59
sanju-react <154334697+sanju-react@users.noreply.github.com>
2024-03-02 11:52:23 +0530

    Update Engagement page with new routes and components

---
+++ src/pages/engagement/EngagementChild.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductChild from '../../components/ProductChild'
+
+const EngagementChild = () => {
+  return (
+    <>
+    <ProductChild/>
+    </>
+  )
+}
+
+export default EngagementChild
